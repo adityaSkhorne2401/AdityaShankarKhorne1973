6622333c26c4ede95c2b1473cc191e1d4e029981
Aditya Shankar Khorne <133169568+AdityaSkhorne@users.noreply.github.com>
2023-12-22 00:17:07 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Aditya Shankar Khorne",
+  title: "AIML Developer",
+  email: "adityashankarkhorne@gmail.com",
+  gitHub: "adityaSkhorne2401",
+  instagram: "adityaskhorne?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+  linkedIn: "adityashankarkhorne",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "adityaShankar73",
+  youTube: "@dmadityakhorne6827?si=bKF6RBUPwdtVM9EW",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/aditya's first msft visit.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "my first visit to microsoft";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "my linkedIn profile🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "here's my linkedIn profile, feel free to connect with me and let's talk about tech and life!",
+    url: "https://www.linkedin.com/in/adityashankarkhorne",
   },
   {
-    title: "Web Development for Beginners",
+    title: "my Instagram profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "here's my Instagram profile, feel free to connect with me and let's talk about tech and life!",
+    url: "https://instagram.com/adityaskhorne?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://adityaskhorne2401.github.io/AdityaShankarKhorne1973/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "my You-Tube account",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "here's my You-Tube account, feel free to connect with me and let's talk about tech and life!",
+    url: "https://youtube.com/@dmadityakhorne6827?si=bKF6RBUPwdtVM9EW",
   },
 ];
 
